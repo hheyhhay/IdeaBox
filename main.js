6e44badceabc74e832b5ff4b9f5706cf26169abd
Natalie McIntyre <nataliemcintyre2021@gmail.com>
2021-06-05 16:51:36 -0700

    updateFavoriteButtonFunctionality

--- main.js
+++ main.js
@@ -8,10 +8,12 @@ var bodyInput = document.querySelector('.js-paragraph');
 var commentText = document.querySelector('.comment-area');
 var wholeCard = document.querySelector('.whole-card');
 var commentCardSection = document.querySelector('.comment-card-container');
-
+var starImage = document.querySelector('.star');
+var deleteImage = document.querySelector('.delete');
+var cardTop = document.querySelector('.top-of-comment');
 
 saveBtn.addEventListener('click', savesCard);
-
+starImage.addEventListener('click', favoriteCard);
 //Create a querySelector for the saved button
 //create an eventListener
 //querySelect als the idea title and the idea body field - specifically the .value
@@ -23,11 +25,11 @@ saveBtn.addEventListener('click', savesCard);
 
 saveBtn.disabled = true; //setting button state to disabled
 
-titleInput.addEventListener('input', stateHandle);
-bodyInput.addEventListener('input', stateHandle);
-wholeCard.addEventListener('click')
+titleInput.addEventListener('input', enableButton);
+bodyInput.addEventListener('input', enableButton);
+// wholeCard.addEventListener('click')
 
-function stateHandle() {
+function enableButton() {
   if ((titleInput.value === "") || (bodyInput.value === "")) {
     saveBtn.disabled = true;
     saveBtn.classList.remove('cursor-change');
@@ -45,7 +47,7 @@ function savesCard(){
  savedIdeas.push(currentIdea);
 renderCard();
 clearsInput();
-stateHandle();
+enableButton();
 };
 
 
@@ -55,7 +57,7 @@ function renderCard(){
 var ideaHTML = "";
 for (var i = 0; i<savedIdeas.length; i++){
     ideaHTML += `<div class="whole-card">
-    <div class="top-of-comment">
+    <div class="top-of-comment" data-id="${savedIdeas[i].id}">
     <input type="image" src = "images/star.svg" name="star" class = "btTxt submit" />
     <input type="image" src = "images/delete.svg" name"delete" class = "btTxt submit" />
     </div>
@@ -86,12 +88,30 @@ function clearsInput(){
   //if the (titleInput.value & bodyInput.values) {
 // than titleInput.value & bodyInput.values = none;}
 
+function favoriteStar() {
+  starImage.classList.add('hidden');
+  deleteImage.classList.add('hidden');
+  cardTop.innerHTML += `<input type="image" src = "images/star-active.svg" name="star" class = "btTxt submit star" />
+  <input type="image" src = "images/delete.svg" name"delete" class = "btTxt submit"/>`
+}
 
 
+function favoriteCard() {
+  var favoriteCard = event.target.closest(".top-of-comment")
+  if (favoriteCard) {
+    for (i = 0; i < savedIdeas.length; i++) {
+      if (savedIdeas[i].id === Number(favoriteCard.dataset.id)) {
+        favoriteStar();
+      }
+    }
+  }
+}
 
+function favorite() {
+  if (this.star = true)
+}
 
-
-
+//
 
 
 
